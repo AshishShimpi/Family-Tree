e115a3a4baa9e9e1c9efe8b2425cedf241f6c00d
AshishShimpi <ashishshimpi99@gmail.com>
2023-01-02 22:31:33 +0530

    render fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const multer = require('multer');
 const cors = require('cors');
 const client = require('./sanity');
 const { getDB, connectToDB } = require('./db');
-const fs = require('node:fs');
 
 require('dotenv').config();
 
